135da5c79a99513b6041fe1e8fa8e14f18f353eb
Mark-Kohrman <mark.kohrman@gmail.com>
2020-08-10 21:13:30 -0500

    adding the index action

---
+++ src/views/CandlesIndex.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="candles-index">
+    <h1>All Candles</h1>
+    <div v-for="candle in candles">
+      <h2> {{candle.scent}}</h2>
+      <p>Color: {{candle.color}}</p>
+      <p>Price: {{candle.price}}</p>
+
+    </div>
+
+
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      candles: [],
+    };
+  },
+  created: function () {
+    axios.get("/api/candles").then((response) => {
+      console.log("candles index", response);
+      this.candles = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
